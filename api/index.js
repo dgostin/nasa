ef7ef4f9d035940b76e659a56155e24aeb713b0a
David Gostin <dgostin@gmail.com>
2024-10-08 21:15:48 -0400

    renamed server.js to index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ require("dotenv").config();
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
+
+console.log("Alive");
 
 // API route (example)
 app.get("/api/hello", (req, res) => {
@@ -13,9 +15,9 @@ app.get("/api/hello", (req, res) => {
 });
 
 // Catch-all route to serve the React app for any non-API request
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
