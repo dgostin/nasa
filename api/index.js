fc18938d9c943401f1794cd16cac83100631f3db
David Gostin <dgostin@gmail.com>
2024-10-09 07:31:46 -0400

    changed vercel.json

--- api/index.js
+++ api/index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-var fs = require("fs");
-var files = fs.readdirSync(path.join(__dirname, "../client"));
+// var fs = require("fs");
+// var files = fs.readdirSync(path.join(__dirname, "../client"));
 
 const app = express();
 
 // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-console.log(files);
+// console.log(files);
 
 // API route (example)
 app.get("/api/hello", (req, res) => {
-  res.json({ message: files });
+  res.json({ message: "Here now" });
 });
 
 // Catch-all route to serve the React app for any non-API request
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
